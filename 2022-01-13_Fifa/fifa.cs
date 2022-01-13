278b1adf8e6c9f61a9bef895e3deb468b481ecb2
FeederWolf <Kollarg22@gmail.com>
2022-01-13 09:38:12 +0100

    1. - 2. feladat

---
+++ 2022-01-13_Fifa/fifa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_13_Fifa
+{
+    class fifa
+    {
+        public string csapat { get; set; }
+        public int helyezes { get; set; }
+        public int valtozas { get; set; }
+        public int pontszam { get; set; }
+        public fifa(string sor)
+        {
+            string[] s = sor.Split(';');
+            csapat = s[0];
+            helyezes = int.Parse(s[1]);
+            valtozas = int.Parse(s[2]);
+            pontszam = int.Parse(s[3]);
+        }
+    }
+
+}
---
+++ 2022-01-13_Fifa/fifavilagranglista.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace _2022_01_13_Fifa
+{
+    class fifavilagranglista
+    {
+        static void Main(string[] args)
+        {
+            List<fifa> fifakxd = new List<fifa>();
+            foreach (var sor in File.ReadAllLines("fifa.txt").Skip(1))
+            {
+                fifakxd.Add(new fifa(sor));
+            }
+            
+        }
+    }
+}
